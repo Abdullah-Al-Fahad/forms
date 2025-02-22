ed765401befe1f453bf6b6d566fa21add0b169fb
Abdullah Al Fahad <abdullah.al.fahad@outlook.com>
2025-02-22 21:32:04 +0600

    updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS Configuration
 app.use(cors({
-  origin: '*',
+  origin: ['http://localhost:5173', 'https://forms-p3wmv8nji-abdullah-al-fahads-projects.vercel.app'], // Add Vercel URL
   methods: '*',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
